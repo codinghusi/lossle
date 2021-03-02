b7dfc56cd6b86dcbb5cab8c676aa6a809b6f82bb
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2021-03-03 00:11:59 +0100

    added reverse proxy

--- dgraph/init.sh
+++ dgraph/init.sh
@@ -4,7 +4,7 @@ check_file=./initialized
 
 while [ ! -f "$check_file" ];
 do
-    sleep 10ss
+    sleep 10s
     if curl -X POST 'http://localhost:8080/admin/schema' --data-binary '@database.graphql' | grep 'Success'
     then
         echo "\n------------------ initialized graphql schema ------"
