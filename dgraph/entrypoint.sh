487c82a5069787b462e8fe49439bdef936d9f509
Gerrit <gerrit.weiermann@gmx.de>
2021-03-14 13:02:46 +0100

    made command a one liner because fuck u

--- dgraph/entrypoint.sh
+++ dgraph/entrypoint.sh
@@ -5,13 +5,4 @@ cd /dgraph
 
 cd /dgraph-data
 # dgraph alpha --my=alpha:7080 --zero=zero:5080 --graphql_lambda_url=http://lambda:8686/graphql_worker --whitelist=0.0.0.0,127.0.0.1
-dgraph alpha --zero=zero:5080 \
-             --expose_trace \
-             --profile_mode block \
-             --block_rate 10 \
-             --logtostderr \
-             -v=2 \
-             --my=alpha:7080 \
-             --security "whitelist=0.0.0.0/0" \
-             --graphql "lambda-url=http://lambda:8686/graphql-worker;" \
-             --trace "ratio=1.0;" \
+dgraph alpha --zero=zero:5080 --expose_trace --profile_mode block --block_rate 10 --logtostderr -v=2 --my=alpha:7080 --security "whitelist=0.0.0.0/0" --graphql "lambda-url=http://lambda:8686/graphql-worker;" --trace "ratio=1.0;"
